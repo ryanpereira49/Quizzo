e99a31b779577bd90e1b7fa493bf353fe6615648
paradox4994 <ryanpereira499@gmail.com>
2025-05-13 18:56:34 -0400

    Added local proxying for development

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig(({ mode }) => {
+  return {
+    plugins: [react()],
+    server: mode === 'development' ? {
+      proxy: {
+        '/api': {
+          target: 'http://localhost:3000', // or your backend dev server
+          changeOrigin: true,
+        },
+      },
+    } : undefined,
+  }
 })
