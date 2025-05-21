8c03957d2f2124fb8d442f8575268ca81a0d62d1
paradox4994 <ryanpereira499@gmail.com>
2025-05-20 23:51:28 -0400

    Removed strict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,9 +5,7 @@ import App from './App.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
     <Router>
       <App />
     </Router>
-  </StrictMode>,
 )
