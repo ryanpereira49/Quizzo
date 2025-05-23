65d11e0570753f0e0d3d72fdff097a6fe48c9b1a
paradox4994 <ryanpereira499@gmail.com>
2025-05-23 03:02:32 -0400

    Added disable option

--- client/src/components/ButtonGlass.tsx
+++ client/src/components/ButtonGlass.tsx
@@ -5,11 +5,12 @@ type ButtonProps = {
     onClick?: () => void;
     type?: 'button' | 'submit' | 'reset';
     className?: string;
+    disabled?: boolean
 }
 
-const ButtonGlass = ({ children, onClick, type = 'button', className = '' }: ButtonProps) => {
+const ButtonGlass = ({ children, onClick, type = 'button', className = '', disabled }: ButtonProps) => {
   return (
-    <button type={type} onClick={onClick} className={`bg-white rounded-lg px-4 py-2 font-bold ${className}`}>
+    <button type={type} onClick={onClick} disabled={disabled} className={`bg-white rounded-lg px-4 py-2 font-semibold disabled:bg-gray-500 disabled:text-gray-700 ${className}`}>
       {children}
     </button>
   );
